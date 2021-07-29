26157fdc5e7dcea1914438abc4df19468870bd35
Ali <ali.khan5317@gmail.com>
2021-07-29 04:41:23 -0400

    Initialize firebase and cleanup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBSDVQtWLNMfNlLZj35kX5YB99TNs0j_vc",
+    authDomain: "facebook-clone-6860b.firebaseapp.com",
+    projectId: "facebook-clone-6860b",
+    storageBucket: "facebook-clone-6860b.appspot.com",
+    messagingSenderId: "781595974397",
+    appId: "1:781595974397:web:349f802c2e48ac0ced3c94",
+    measurementId: "G-PPZ4WHDEKK"
+  };
