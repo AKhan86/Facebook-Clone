fbb17ec7a84869f094855b2063b2c892ae11a38b
Ali <ali.khan5317@gmail.com>
2021-07-29 06:21:24 -0400

    Add styling to header

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,14 @@ import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
 import FlagIcon from '@material-ui/icons/Flag';
-import { StorefrontOutlined, SubscriptionsOutlined, SupervisedUserCircle } from '@material-ui/icons';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import { Avatar , IconButton } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 function Header() {
     return (
@@ -24,10 +31,10 @@ function Header() {
                     <FlagIcon fontSize="large" />
                 </div>
                 <div className="header_option">
-                    <SubscriptionsOutlinedIcon fontSize="large" />
+                    <SubscriptionsIcon fontSize="large" />
                 </div>
                 <div className="header_option">
-                    <StorefrontOutlinedIcon fontSize="large" />
+                    <StorefrontIcon fontSize="large" />
                 </div>
                 <div className="header_option">
                     <SupervisedUserCircleIcon fontSize="large" />
@@ -35,7 +42,23 @@ function Header() {
             </div>
 
             <div className="header_right">
-                
+                <div className="header_info">
+                    <Avatar />
+                    <h4>Ali Khan</h4>
+                </div>
+
+                <IconButton>
+                    <AddIcon />
+                </IconButton>
+                <IconButton>
+                    <ForumIcon />
+                </IconButton>
+                <IconButton>
+                    <NotificationsActiveIcon />
+                </IconButton>
+                <IconButton>
+                    <ExpandMoreIcon />
+                </IconButton>
             </div>
         </div>
     );
